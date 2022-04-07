286a43e2fd7ca0a7e9ff70fb23ef5754011549fc
Vianney <vianneyyovo@gmail.com>
2022-04-07 14:10:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
@@ -12,4 +13,3 @@ int main(void)
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
 }
-
