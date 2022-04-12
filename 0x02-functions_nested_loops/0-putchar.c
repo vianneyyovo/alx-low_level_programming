da65b3cb6fecbacfc530865c0319ce0c6fe80e4a
vianney yovo <vianneyyovo@gmail.com>
2022-04-12 18:59:45 +0000

    0-commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include "main.ch"
+#include "main.h"
 
 /**
- * main - print _putchar
+ * main - prints _putchar
  *
  * Description: prints _putchar
  *
- * Return: 0 (Sucess)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char text[9] = "_putchar;
+	char text[9] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
