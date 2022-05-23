9c18304644a62f68145c4c93964f3ea93448ef18
vianney yovo <vianneyyovo@gmail.com>
2022-05-23 20:53:44 +0000

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,50 @@
 /*
- * File: 10-delete_nodeint.c
- * Author: Yovo Koffi Vianney
+* Fichier : 9-insert_nodeint.c
+* Auteur : Yov Koffi Vianney
  */
 
-#include "lists.h"
+# inclure  " listes.h "
 
-/**
- * delete_nodeint_at_index - Deletes the node at a given
- *                           index of a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- * @index: The index of the node to be deleted - indices start at 0.
- *
- * Return: On success - 1.
- *         On failure - -1.
+/* *
+* insert_nodeint_at_index - Insère un nouveau nœud dans un listint_t
+* liste à une position donnée.
+* @head : Un pointeur vers l'adresse du
+* tête de la liste listint_t.
+* @idx : L'index de la liste listint_t où le nouveau
+* le nœud doit être ajouté - les indices commencent à 0.
+* @n : L'entier que le nouveau nœud doit contenir.
+*
+* Retour : Si la fonction échoue - NULL.
+* Sinon - l'adresse du nouveau nœud.
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t * insert_nodeint_at_index ( listint_t **head, unsigned  int idx, int n)
 {
-	listint_t *tmp, *copy = *head;
-	unsigned int node;
+	listint_t *nouveau, *copie = *tête ;
+	 nœud int non signé ;
 
-	if (copy == NULL)
-		return (-1);
+	new = malloc ( sizeof ( listint_t ));
+	si (nouveau == NULL )
+		retour ( NULL );
 
-	if (index == 0)
+	nouveau-> n = n ;
+
+	si (idx == 0 )
 	{
-		*head = (*head)->next;
-		free(copy);
-		return (1);
+		nouveau-> suivant = copier ;
+		*tête = nouveau ;
+		retour (nouveau);
 	}
 
-	for (node = 0; node < (index - 1); node++)
+	pour (noeud = 0 ; noeud < (idx - 1 ); noeud++)
 	{
-		if (copy->next == NULL)
-			return (-1);
+		si (copie == NULL || copie-> suivant == NULL )
+			retour ( NULL );
 
-		copy = copy->next;
+		copier = copier-> suivant ;
 	}
 
-	tmp = copy->next;
-	copy->next = tmp->next;
-	free(tmp);
-	return (1);
+	nouveau-> suivant = copier-> suivant ;
+	copier-> suivant = nouveau ;
+
+	retour (nouveau);
 }
